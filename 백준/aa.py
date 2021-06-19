fff566af6a34a21cad95e73b88f42ba5edbc7f96
leekihoon <leekihoon1689@gmail.com>
2021-06-19 14:56:17 +0900

    210619

---
+++ 백준/aa.py
@@ -0,0 +1,6 @@
+A, B = map(int, input().split())
+C = A + B
+if C > 100 :
+    print("ITS DONE")
+if A > 45 :
+    print("ITS PENDING")
---
+++ 백준/no10773.py
@@ -0,0 +1,2 @@
+array = {0} in range(100000)
+def puts(num) :
