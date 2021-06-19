2deeb314f729df8ee42c23419144b4281b53e1e4
leekihoon <leekihoon1689@gmail.com>
2021-06-19 16:32:56 +0900

    210619

--- 백준/no10773.py
+++ 백준/no10773.py
@@ -1,2 +1,24 @@
-array = {0} in range(100000)
+arrayList = []
+
 def puts(num) :
+    arrayList.append(num)
+
+def pops() :
+    arrayList.pop()
+
+# def numberSum() :
+#     for a in arrayList :
+#         sumNum22 = sumNum22 + a
+#     print(sumNum22)
+
+
+T = int(input())
+for i in range(T):
+    number = int(input())
+    if number != 0 :
+        puts(number)
+    else :
+        pops()
+
+    if i == T-1:
+        print(sum(arrayList))
