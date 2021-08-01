855ff2418b2fccd667d3b482323ea58ae7e3d4ae
leekihoon <leekihoon1689@gmail.com>
2021-08-01 17:45:33 +0900

    210801

---
+++ 백준/no1021.py
@@ -0,0 +1,34 @@
+import sys
+import collections 
+
+def first() :
+    array.popleft()
+
+def second() :
+    global count
+    temp = array.popleft()
+    array.append(temp)
+    count += 1
+
+def third() :
+    global count
+    temp = array.pop()
+    array.appendleft(temp)
+    count += 1
+    
+count = 0
+
+n, m = map(int, sys.stdin.readline().split())
+sequnceArray = list(map(int, sys.stdin.readline().split()))
+
+array = collections.deque(_ for _ in range(1,n+1))
+
+for i in sequnceArray :
+    while i != array[0] :
+        if array.index(i) < len(array)/2 :
+            second()
+        else :
+            third()
+    first()
+
+print(count)
