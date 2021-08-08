b885a3406c6b3f0b71fc508d7edb30700ec97415
leekihoon <leekihoon1689@gmail.com>
2021-08-08 19:16:52 +0900

    210808

--- 백준/no5430.py
+++ 백준/no5430.py
@@ -1,64 +1,48 @@
-<<<<<<< HEAD
 import sys
 import collections
-import numpy as np
 
-T = int(sys.stdin.readline())
-for _ in range(T) :
-    FuncArray = sys.stdin.readline()
-    n = int(sys.stdin.readline())
-    inputArray = sys.stdin.readline()[1:-2].split(",")
-    deque = collections.deque(inputArray)
+for _ in range(int(sys.stdin.readline())) :
+    FuncArray = sys.stdin.readline() #RDD
+    n = int(sys.stdin.readline()) #4
+    inputArray = sys.stdin.readline()[1:-2].split(",") #1,2,3,4
+    if inputArray[0] != '' :
+        deque = collections.deque(inputArray) # deque[1,2,3,4]
+    else :
+        deque = collections.deque()
 
-    TF = True
+    
+    leftcount= 0
+    rightcount = 0
+    error = False
+    TF = True #TF == True > 리버스x,  TF == False > 리버스ㅇ
 
-    for i in FuncArray :
-        if i == 'R' :
+    for i in FuncArray : # i == R or D
+        
+        if i == 'R' : #리버스
             if TF == True : TF = False
             else : TF = True
-        else :
-            if len(deque) == 0 :
-                print("error")
-                continue
-            if TF == True :
-                deque.popleft()
-            elif TF == False :
-                deque.pop()
 
-    if FuncArray.count('R') %2 != 0 :
+        elif i == 'D' : #Drop
+            if  len(deque) == 0 : #요소가 없으면
+                error = True  #error 출력하고 리셋
+                break
+            else :
+                if TF == True :
+                    #leftcount +=1
+                    deque.popleft()
+                elif TF == False :
+                    #rightcount +=1
+                    deque.pop()
+
+    if FuncArray.count('R') %2 != 0 : # 리버스가 홀수면 
         deque.reverse()
 
-    print(deque) 
-=======
-import sys
-import collections
-import numpy as np
-
-T = int(sys.stdin.readline())
-for _ in range(T) :
-    FuncArray = sys.stdin.readline()
-    n = int(sys.stdin.readline())
-    inputArray = sys.stdin.readline()[1:-2].split(",")
-    deque = collections.deque(inputArray)
-
-    TF = True
-
-    for i in FuncArray :
-        if i == 'R' :
-            if TF == True : TF = False
-            else : TF = True
-        else :
-            if len(deque) == 0 :
-                print("error")
-                continue
-            if TF == True :
-                deque.popleft()
-            elif TF == False :
-                deque.pop()
-
-    if FuncArray.count('R') %2 != 0 :
-        deque.reverse()
-
-    print(deque) 
-    #
->>>>>>> 03d1feb23eafa0ffd6e61de7a681836c5d229eff
+    if error :
+        print("error")
+    else :
+        print("[",end="")
+        for i in range(len(deque)):
+            print(deque[i],end="")
+            if i != len(deque)-1:
+                print(",",end="")
+        print("]")
